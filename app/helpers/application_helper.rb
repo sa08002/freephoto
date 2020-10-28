012c3d95c93d76d9ea39ad6adba51c339c185191
大西幹則 <1078sa08002@gmail.com>
2020-10-28 15:08:07 +0900

    プロフィールページの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # def avatar_url(user)
+  #   return user.profile_photo unless user.profile_photo.nil?
+  #   gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+  #   "https://qiita-user-contents.imgix.net/https%3A%2F%2Fwww.gravatar.com%2Favatar%2F00000000000000000000000000000000%3Fd%3Dmp?ixlib=rb-1.2.2&auto=format&gif-q=60&q=75&s=cc308fe2b132632f608891adf96b3f44"
+  # end
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
 end
